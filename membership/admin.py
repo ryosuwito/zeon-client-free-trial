f1282a007721542717d190b857d290d3038b2271
ryosuwito <ryosuwito.grades@gmail.com>
2018-09-19 14:18:45 +0700

    add_staff_model

--- membership/admin.py
+++ membership/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Member, PackageGroup
+from .models import Member, PackageGroup, Staff
 
 class PackageGroupAdmin(admin.ModelAdmin):
     model = PackageGroup
@@ -8,5 +8,10 @@ class MemberAdmin(admin.ModelAdmin):
     model = Member
     exclude = ['slug']
 
+class StaffAdmin(admin.ModelAdmin):
+    model = Staff
+    exclude = ['slug']
+
 admin.site.register(PackageGroup, PackageGroupAdmin)
 admin.site.register(Member, MemberAdmin)
+admin.site.register(Staff, StaffAdmin)
