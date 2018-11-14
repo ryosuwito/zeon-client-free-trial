3e8a1454e71b225dced28ca1d8a7a55cb52cc1cf
kemong <kemong.dev123@gmail.com>
2018-11-14 23:45:49 +0700

    local

--- dispatcher/views.py
+++ dispatcher/views.py
@@ -82,8 +82,8 @@ class Index(Dispatcher):
             template = "company_profile/%s/index.html"%(configs.templates.dir_name)
 
         all_articles = ArticleModel.objects.filter(site=site, is_published=True).order_by('-created_date')
-        if len(all_articles)>=12:
-            recent_articles = all_articles[:12]
+        if len(all_articles)>=36:
+            recent_articles = all_articles[:36]
         else:
             recent_articles = all_articles
 
